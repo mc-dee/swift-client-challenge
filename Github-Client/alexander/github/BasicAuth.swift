b7d33c3bd4c7a52693c342fa7d6c3560bec6d5bf
Alexander Elbracht <alexander@elbracht.org>
2016-03-24 17:08:10 +0100

    Fix issue with empty username and password

--- Github-Client/alexander/github/BasicAuth.swift
+++ Github-Client/alexander/github/BasicAuth.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class BasicAuth: NSObject {
     static func sessionConfiguration() -> NSURLSessionConfiguration {
-        let username = "username"
-        let password = "password or token"
+        let username = ""
+        let password = ""
         let loginString = NSString(format: "%@:%@", username, password)
         let loginData: NSData = loginString.dataUsingEncoding(NSUTF8StringEncoding)!
         let base64LoginString = loginData.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
         let authString = "Basic \(base64LoginString)"
         let config = NSURLSessionConfiguration.defaultSessionConfiguration()
-        config.HTTPAdditionalHeaders = ["Authorization" : authString]
+        if !username.isEmpty && !password.isEmpty {
+            config.HTTPAdditionalHeaders = ["Authorization" : authString]
+        }
+        
         return config
     }
 }
